744f176323fb33912d5144b3a2a3e6840b2c1736
Jeferson Belgamazzi <jbelgamazzi@users.noreply.github.com>
2015-11-21 14:57:27 -0200

    Update script.js

--- Cube3DWebGL/script.js
+++ Cube3DWebGL/script.js
@@ -53,6 +53,11 @@ var main = function () {
     CANVAS.addEventListener("mouseout", mouseUp, false);
     CANVAS.addEventListener("mouseup", mouseUp, false);
     
+    CANVAS.addEventListener("touchstart", mouseDown, false);
+    CANVAS.addEventListener("touchmove", mouseMove, false);
+    CANVAS.addEventListener("touchcancel", mouseUp, false);
+    CANVAS.addEventListener("touchleave", mouseUp, false);
+    CANVAS.addEventListener("touchend", mouseUp, false);
 
     /** Obtem contexto WebGL **/
     // esta variavel é o ponto de acesso com a lib
