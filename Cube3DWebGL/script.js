6329897a0e2101a73e4ade5be93f21db27e12916
Jeferson Belgamazzi <jbelgamazzi@users.noreply.github.com>
2015-11-21 15:46:37 -0200

    Update script.js

--- Cube3DWebGL/script.js
+++ Cube3DWebGL/script.js
@@ -53,11 +53,119 @@ var main = function () {
     CANVAS.addEventListener("mouseout", mouseUp, false);
     CANVAS.addEventListener("mouseup", mouseUp, false);
     
-    //CANVAS.addEventListener("touchstart", mouseDown, false);
-    CANVAS.addEventListener("touchmove", mouseMove, false);
-    //CANVAS.addEventListener("touchcancel", mouseUp, false);
-    //CANVAS.addEventListener("touchleave", mouseUp, false);
-    //CANVAS.addEventListener("touchend", mouseUp, false);
+    var ongoingTouches = new Array; 
+    
+    function handleStart(evt) {
+        evt.preventDefault();
+        log("touchstart.");
+        var el = CANVAS[0];
+        var ctx = el.getContext("2d");
+        var touches = evt.changedTouches;
+        
+        for (var i=0; i < touches.length; i++) {
+            log("touchstart:"+i+"...");
+            ongoingTouches.push(copyTouch(touches[i]));
+            var color = colorForTouch(touches[i]);
+            ctx.beginPath();
+            ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0,2*Math.PI, false);  // a circle at the start
+            ctx.fillStyle = color;
+            ctx.fill();
+            log("touchstart:"+i+".");
+        }
+    }
+    
+    /// rastreando novos toques
+    function ongoingTouchIndexById(idToFind) {
+      for (var i=0; i < ongoingTouches.length; i++) {
+        var id = ongoingTouches[i].identifier;
+        
+        if (id == idToFind) {
+          return i;
+        }
+      }
+      return -1;    // não econtrado
+    }
+    
+    
+    function handleMove(evt) {
+  evt.preventDefault();
+  var el = CANVAS[0];
+  var ctx = el.getContext("2d");
+  var touches = evt.changedTouches;
+
+  for (var i=0; i < touches.length; i++) {
+    var color = colorForTouch(touches[i]);
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if(idx >= 0) {
+      log("continuing touch "+idx);
+      ctx.beginPath();
+      log("ctx.moveTo("+ongoingTouches[idx].pageX+", "+ongoingTouches[idx].pageY+");");
+      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+      log("ctx.lineTo("+touches[i].pageX+", "+touches[i].pageY+");");
+      ctx.lineTo(touches[i].pageX, touches[i].pageY);
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = color;
+      ctx.stroke();
+
+      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+      log(".");
+    } else {
+      log("can't figure out which touch to continue");
+    }
+  }
+}
+    
+    
+    function handleEnd(evt) {
+      evt.preventDefault();
+      log("touchend/touchleave.");
+      var el = CANVAS[0];
+      var ctx = el.getContext("2d");
+      var touches = evt.changedTouches;
+    
+      for (var i=0; i < touches.length; i++) {
+        var color = colorForTouch(touches[i]);
+        var idx = ongoingTouchIndexById(touches[i].identifier);
+    
+        if(idx >= 0) {
+          ctx.lineWidth = 4;
+          ctx.fillStyle = color;
+          ctx.beginPath();
+          ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+          ctx.lineTo(touches[i].pageX, touches[i].pageY);
+          ctx.fillRect(touches[i].pageX-4, touches[i].pageY-4, 8, 8);  // and a square at the end
+          ongoingTouches.splice(idx, 1);  // remove it; we're done
+        } else {
+          log("can't figure out which touch to end");
+        }
+      }
+    }
+    
+    function handleCancel(evt) {
+      evt.preventDefault();
+      log("touchcancel.");
+      var touches = evt.changedTouches;
+      
+      for (var i=0; i < touches.length; i++) {
+        ongoingTouches.splice(i, 1);  // remove it; we're done
+      }
+    }
+    
+    function copyTouch(touch) {
+      return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
+    }
+    
+    function log(msg) {
+        console.log(msg);
+      //var p = document.getElementById('log');
+      //p.innerHTML = msg + "\n" + p.innerHTML;
+    }
+    CANVAS.addEventListener("touchstart", handleStart, false);
+    CANVAS.addEventListener("touchend", handleEnd, false);
+    CANVAS.addEventListener("touchcancel", handleCancel, false);
+    CANVAS.addEventListener("touchleave", handleEnd, false);
+    CANVAS.addEventListener("touchmove", handleMove, false);
 
     /** Obtem contexto WebGL **/
     // esta variavel é o ponto de acesso com a lib
